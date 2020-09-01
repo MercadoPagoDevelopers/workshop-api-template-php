3fc34522beaca126767acf0abcb9587d58b69718
coco <coco@lacueva.tv>
2020-09-01 11:22:56 -0300

    prueba error

--- process_payment.php
+++ process_payment.php
@@ -31,3 +31,40 @@
     echo json_encode($response);
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Compraste</title>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+</head>
+<body>
+    
+    <div class="container">
+    
+        <?php if ($response['status'] === 'approved' ) : ?>
+            <h1>
+                Su compra ha sido realizada. Gracias, vuelva prontos!
+            </h1>
+
+            <p>Detalles: <?php echo $response['status_detail']; ?></p>
+
+        <?php else : ?>
+            <h1>
+                Fracaso absoluto
+            </h1>
+
+            <p>
+                Intente otra vez.
+            </p>
+
+            <p>Detalles: <?php echo $response['status_detail']; ?></p>
+
+        <?php endif; ?>
+    </div>
+
+
+</body>
+</html>
