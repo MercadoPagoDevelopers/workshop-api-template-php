ca2e3b6171118e9d528fee0c8200877a2b715ea0
coco <coco@lacueva.tv>
2020-09-01 10:31:42 -0300

    lock

--- detail.php
+++ detail.php
@@ -1,3 +1,27 @@
+<?php
+
+    // require_once 'vendor/autoload.php';
+
+    // MercadoPago\SDK::setAccessToken("ENV_ACCESS_TOKEN");
+
+    // $payment = new MercadoPago\Payment();
+
+    // $payment->transaction_amount = 141;
+    // $payment->token = "YOUR_CARD_TOKEN";
+    // $payment->description = "Ergonomic Silk Shirt";
+    // $payment->installments = 1;
+    // $payment->payment_method_id = "visa";
+    // $payment->payer = array(
+    //   "email" => "larue.nienow@hotmail.com"
+    // );
+
+    // $payment->save();
+
+    // echo $payment->status;
+
+    var_dump($_POST);
+
+?>
 <!DOCTYPE html>
 <html>
     <head>
